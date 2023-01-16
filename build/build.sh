7b1cca730cef4e431f37116aad7b9f912c611461
TheTaylorLee <53202926+TheTaylorLee@users.noreply.github.com>
2023-01-16 10:15:45 -0600

    update build file

--- build/build.sh
+++ build/build.sh
@@ -1,14 +1,19 @@
 # CI/CD for maintainers only and for those trying to build their own image from the source
-## BuildImages
+## Build Dev Images
 cd ~
 rm docker-transcodeautomation -d -r
 git clone https://github.com/TheTaylorLee/docker-transcodeautomation
 cd docker-transcodeautomation
 version=v2.7.0
+DOCKER_BUILDKIT=1 docker build -f Dockerfile.ubuntu22.04-lts -t ttlee/docker-transcodeautomation:ubuntu22.04-lts-develop .
+DOCKER_BUILDKIT=1 docker build -f Dockerfile.alpine3.14-lts -t ttlee/docker-transcodeautomation:alpine3.14-lts-develop .
+
+### Build Prod
 DOCKER_BUILDKIT=1 docker build -f Dockerfile.ubuntu22.04-lts -t ttlee/docker-transcodeautomation:ubuntu22.04-lts-$version .
 DOCKER_BUILDKIT=1 docker build -f Dockerfile.alpine3.14-lts -t ttlee/docker-transcodeautomation:alpine3.14-lts-$version .
 DOCKER_BUILDKIT=1 docker build -f Dockerfile.ubuntu22.04-lts -t ttlee/docker-transcodeautomation:ubuntu22.04-lts .
 DOCKER_BUILDKIT=1 docker build -f Dockerfile.alpine3.14-lts -t ttlee/docker-transcodeautomation:alpine3.14-lts .
+
 
 ### Pushing the images
 docker push ttlee/docker-transcodeautomation -a
