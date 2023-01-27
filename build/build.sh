0012d3117bf6bec23a834e1a2596c923f2c6ea16
TheTaylorLee <53202926+TheTaylorLee@users.noreply.github.com>
2023-01-26 23:26:59 -0600

    move to ghcr

--- build/build.sh
+++ build/build.sh
@@ -2,24 +2,27 @@
 ## Build Dev Images
 cd ~
 rm docker-transcodeautomation -d -r
-git clone https://github.com/TheTaylorLee/docker-transcodeautomation
+git clone https://github.com/thetaylorlee/docker-transcodeautomation
 cd docker-transcodeautomation
 version=v2.8.0
-DOCKER_BUILDKIT=1 docker build -f Dockerfile.ubuntu22.04-lts -t ttlee/docker-transcodeautomation:ubuntu22.04-lts-develop .
-DOCKER_BUILDKIT=1 docker build -f Dockerfile.alpine3.14-lts -t ttlee/docker-transcodeautomation:alpine3.14-lts-develop .
+
+DOCKER_BUILDKIT=1 docker build -f Dockerfile.ubuntu22.04-lts -t ghcr.io/thetaylorlee/docker-transcodeautomation:ubuntu22.04-lts-develop .
+DOCKER_BUILDKIT=1 docker build -f Dockerfile.alpine3.14-lts -t ghcr.io/thetaylorlee/docker-transcodeautomation:alpine3.14-lts-develop .
 
 ### Build Prod
-DOCKER_BUILDKIT=1 docker build -f Dockerfile.ubuntu22.04-lts -t ttlee/docker-transcodeautomation:ubuntu22.04-lts-$version .
-DOCKER_BUILDKIT=1 docker build -f Dockerfile.alpine3.14-lts -t ttlee/docker-transcodeautomation:alpine3.14-lts-$version .
-DOCKER_BUILDKIT=1 docker build -f Dockerfile.ubuntu22.04-lts -t ttlee/docker-transcodeautomation:ubuntu22.04-lts .
-DOCKER_BUILDKIT=1 docker build -f Dockerfile.alpine3.14-lts -t ttlee/docker-transcodeautomation:alpine3.14-lts .
-DOCKER_BUILDKIT=1 docker build -f Dockerfile.alpine3.14-lts -t ttlee/docker-transcodeautomation:latest .
+
+DOCKER_BUILDKIT=1 docker build -f Dockerfile.ubuntu22.04-lts -t ghcr.io/thetaylorlee/docker-transcodeautomation:ubuntu22.04-lts-$version .
+DOCKER_BUILDKIT=1 docker build -f Dockerfile.alpine3.14-lts -t ghcr.io/thetaylorlee/docker-transcodeautomation:alpine3.14-lts-$version .
+DOCKER_BUILDKIT=1 docker build -f Dockerfile.ubuntu22.04-lts -t ghcr.io/thetaylorlee/docker-transcodeautomation:ubuntu22.04-lts .
+DOCKER_BUILDKIT=1 docker build -f Dockerfile.alpine3.14-lts -t ghcr.io/thetaylorlee/docker-transcodeautomation:alpine3.14-lts .
+DOCKER_BUILDKIT=1 docker build -f Dockerfile.alpine3.14-lts -t ghcr.io/thetaylorlee/docker-transcodeautomation:latest .
 
 ### Pushing the images
-docker push ttlee/docker-transcodeautomation:ubuntu22.04-lts-develop
-docker push ttlee/docker-transcodeautomation:alpine3.14-lts-develop
-docker push ttlee/docker-transcodeautomation:latest
-docker push ttlee/docker-transcodeautomation:ubuntu22.04-lts-$version
-docker push ttlee/docker-transcodeautomation:alpine3.14-lts-$version
-docker push ttlee/docker-transcodeautomation:ubuntu22.04-lts
-docker push ttlee/docker-transcodeautomation:alpine3.14-lts
+
+docker push ghcr.io/thetaylorlee/docker-transcodeautomation:ubuntu22.04-lts-develop
+docker push ghcr.io/thetaylorlee/docker-transcodeautomation:alpine3.14-lts-develop
+docker push ghcr.io/thetaylorlee/docker-transcodeautomation:latest
+docker push ghcr.io/thetaylorlee/docker-transcodeautomation:ubuntu22.04-lts-$version
+docker push ghcr.io/thetaylorlee/docker-transcodeautomation:alpine3.14-lts-$version
+docker push ghcr.io/thetaylorlee/docker-transcodeautomation:ubuntu22.04-lts
+docker push ghcr.io/thetaylorlee/docker-transcodeautomation:alpine3.14-lts
