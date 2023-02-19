79bca269689417ded44335e99aac9772c85afdf0
TheTaylorLee <53202926+TheTaylorLee@users.noreply.github.com>
2023-02-19 17:22:57 -0600

    2.17.0

--- build/scripts/Set-PSReadlineOptions.ps1
+++
@@ -1,9 +0,0 @@
-Write-Output "[+] Set-PSReadLineOptions Start"
-
-Set-PSReadLineOption -HistorySavePath /docker-transcodeautomation/data/pwsh_history.txt
-Set-PSReadLineOption -BellStyle None -EditMode Windows
-Set-PSReadLineOption -PredictionSource HistoryAndPlugin
-Set-PSReadLineOption -PredictionViewStyle ListView
-Set-PSReadLineKeyHandler -Chord Tab -Function MenuComplete
-
-Write-Output "[+] Set-PSReadLineOptions End"
