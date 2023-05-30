69a1ba612dfeaeaad221089b5d671b359420b32b
TheTaylorLee <53202926+TheTaylorLee@users.noreply.github.com>
2023-05-30 16:45:53 -0500

    2.21.0

--- build/scripts/Update-Database.ps1
+++ build/scripts/Update-Database.ps1
@@ -9,4 +9,9 @@ $Tablename = "StatisticsLive"
 $Query = "CREATE TABLE IF NOT EXISTS $Tablename (tablename TEXT, mediacount NUMERIC, oldsizeMB NUMERIC, newsizeMB NUMERIC, differenceMB NUMERIC, percent NUMERIC, existssumsizeMB NUMERIC, existsoldsizeMB NUMERIC, existsnewsizeMB NUMERIC, existsdifferenceMB NUMERIC, existspercent NUMERIC, added DATETIME, updatedby TEXT, growth30daysMB NUMERIC, growth90daysMB NUMERIC, growth180daysMB NUMERIC, growth365daysMB NUMERIC)"
 Invoke-SqliteQuery -Query $Query -DataSource $DataSource
 
+# Check for and Create UpdateProcessedLog
+$Tablename = "UpdateProcessedLog"
+$Query = "CREATE TABLE IF NOT EXISTS $Tablename (daterun DATETIME)"
+Invoke-SqliteQuery -Query $Query -DataSource $DataSource
+
 Write-Output "[+] Update-Database End"
---
+++ build/scripts/Update-Processed.ps1
@@ -0,0 +1,63 @@
+Write-Output "[+] Update-Processed Script Check Started"
+
+[string]$DataSource = "/docker-transcodeautomation/data/MediaDB.SQLite"
+
+# Test to see if update-processed has run in the last 7 days
+$getcount = Invoke-SqliteQuery -DataSource $DataSource -Query "select * from UpdateProcessedLog"
+[int]$i = $getcount.count - 1
+$queryrun = (Invoke-SqliteQuery -DataSource $DataSource -Query "select * from UpdateProcessedLog")[$i] |
+    Where-Object { $_.daterun -lt (Get-Date).AddDays(-7) }
+
+# If not then run in the last 7 days, then update-processed
+if ($null -eq $queryrun) {
+    $startdt = Get-Date
+    Write-Output "[+] Update-Processed media check begun at $startdt"
+
+    # Movies
+    $tablename = "Movies"
+    $sqlmovies = Invoke-SqliteQuery -DataSource $DataSource -Query "Select * FROM $TableName"
+    foreach ($sqlmovie in $sqlmovies) {
+        $fullname = $sqlmovie.fullname
+        if (Test-Path "$fullname") {
+            $Probe = ffprobe -loglevel 0 -print_format json -show_format $fullname
+            $convert = $Probe | ConvertFrom-Json
+            $comment = $convert.format.tags.comment
+            if ($comment -ne 'transcoded') {
+                $modified = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+                Invoke-SqliteQuery -DataSource $DataSource -Query "Update $TableName set comment = NULL, updatedby = 'Update-Processed', modified = `"$modified`" WHERE fullname = `"$fullname`""
+            }
+        }
+        else {
+            if ($null -ne $sqlshow.comment -or $null -ne $sqlshow.filesizeMB -or $sqlshow.fileexists -ne 'false') {
+                $modified = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+                Invoke-SqliteQuery -DataSource $DataSource -Query "Update $TableName set comment = NULL, updatedby = 'Update-Processed', modified = `"$modified`", filesizeMB = NULL, fileexists = 'false' WHERE fullname = `"$fullname`""
+            }
+        }
+    }
+
+    # Shows
+    $tablename = "Shows"
+    $sqlshows = Invoke-SqliteQuery -DataSource $DataSource -Query "Select * FROM $TableName"
+    foreach ($sqlshow in $sqlshows) {
+        $fullname = $sqlshow.fullname
+        if (Test-Path "$fullname") {
+            $Probe = ffprobe -loglevel 0 -print_format json -show_format $fullname
+            $convert = $Probe | ConvertFrom-Json
+            $comment = $convert.format.tags.comment
+            if ($comment -ne 'transcoded') {
+                $modified = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+                Invoke-SqliteQuery -DataSource $DataSource -Query "Update $TableName set comment = NULL, updatedby = 'Update-Processed', modified = `"$modified`" WHERE fullname = `"$fullname`""
+            }
+        }
+        else {
+            if ($null -ne $sqlshow.comment -or $null -ne $sqlshow.filesizeMB -or $sqlshow.fileexists -ne 'false') {
+                $modified = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+                Invoke-SqliteQuery -DataSource $DataSource -Query "Update $TableName set comment = NULL, updatedby = 'Update-Processed', modified = `"$modified`", filesizeMB = NULL, fileexists = 'false' WHERE fullname = `"$fullname`""
+            }
+        }
+    }
+    $enddt = Get-Date
+    Write-Output "[+] Update-Processed media check ended at $enddt"
+}
+
+Write-Output "[+] Update-Processed Script Check Ended"
