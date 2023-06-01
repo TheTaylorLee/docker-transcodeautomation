b5b3edfbd0ca0d24ef9dba4b010f9546042aa1da
TheTaylorLee <53202926+TheTaylorLee@users.noreply.github.com>
2023-06-01 15:33:51 -0500

    2.22.0

--- build/Invoke-TranscodeAutomation.ps1
+++ build/Invoke-TranscodeAutomation.ps1
@@ -1,52 +1,53 @@
-#Used in Debug Logs
-$time = Get-Date -Format "yyyy-MM-dd HHmmss"
-Start-Transcript "$PSScriptRoot/data/logs/$time debug.log"
-Write-Output "[+] TranscodeAutomation Start"
-
-#Debug log management
-Get-ChildItem -Path $PSScriptRoot/data/logs/ |
-    Where-Object { $_.CreationTime -lt (Get-Date).AddDays(-14) } |
-    Remove-Item
-
-# Fix for environment variables not being pulled in by the service
-$env:FFToolsSource = "/docker-transcodeautomation/transcoding/"
-$env:FFToolsTarget = "/docker-transcodeautomation/transcoding/new/"
-
-# Create directories if missing
-New-Item /docker-transcodeautomation/transcoding/new/ -ItemType Directory -ErrorAction silentlycontinue -Verbose
-New-Item /docker-transcodeautomation/transcoding/new/recover -ItemType Directory -ErrorAction silentlycontinue -Verbose
-New-Item /docker-transcodeautomation/transcoding/new/processed -ItemType Directory -ErrorAction silentlycontinue -Verbose
-New-Item /docker-transcodeautomation/data/logs -ItemType Directory -ErrorAction silentlycontinue -Verbose
-New-Item /docker-transcodeautomation/data/Backups -ItemType Directory -ErrorAction silentlycontinue -Verbose
-
-# Import PSSqlite
-Import-Module /root/.local/share/powershell/Modules/PSSQLite/1.1.0/PSSQLite.psm1 -ErrorAction Stop
-
-# Test for existence of media database
-$datasource = ("/docker-transcodeautomation/data/MediaDB.SQLite")
-$test3 = Test-Path $datasource
-if ($test3 -eq $false) {
-    Write-Output "[+] Creating sqlite database"
-    . $PSScriptRoot/functions/Invoke-DBSetup.ps1
-    Invoke-DBSetup -DataSource "/docker-transcodeautomation/data/MediaDB.SQLite"
-}
-
-# Update the database for missing tables added in new versions of docker-transcodeautomation
-/docker-transcodeautomation/scripts/Update-Database.ps1
-
-# Create PS Drive for available free space checking
-New-PSDrive -Name transcoding -Root /docker-transcodeautomation/transcoding -PSProvider FileSystem
-
-# Check for required variables
-if ($null -eq $env:BACKUPPROCESSED -or $null -eq $env:BACKUPRETENTION -or $null -eq $env:MEDIAMOVIEFOLDERS -or $null -eq $env:MEDIASHOWFOLDERS) {
-    Write-Warning "[-] Required environment Variable not set. Review the README documentation for help. Processing will not continue."
-    while ($true) {
-        Start-Sleep -Seconds 2147483
-    }
-}
-
-# Import transcode automation scripts and continue with transcode automation
 if ($host.version.major -eq '7') {
+    # Setup Environment
+    ##Used in Debug Logs
+    $time = Get-Date -Format "yyyy-MM-dd HHmmss"
+    Start-Transcript "$PSScriptRoot/data/logs/$time debug.log"
+    Write-Output "[+] TranscodeAutomation Start"
+
+    ##Debug log management
+    Get-ChildItem -Path $PSScriptRoot/data/logs/ |
+        Where-Object { $_.CreationTime -lt (Get-Date).AddDays(-14) } |
+        Remove-Item
+
+    ##Fix for environment variables not being pulled in by the service
+    $env:FFToolsSource = "/docker-transcodeautomation/transcoding/"
+    $env:FFToolsTarget = "/docker-transcodeautomation/transcoding/new/"
+
+    ##Create directories if missing
+    New-Item /docker-transcodeautomation/transcoding/new/ -ItemType Directory -ErrorAction silentlycontinue -Verbose
+    New-Item /docker-transcodeautomation/transcoding/new/recover -ItemType Directory -ErrorAction silentlycontinue -Verbose
+    New-Item /docker-transcodeautomation/transcoding/new/processed -ItemType Directory -ErrorAction silentlycontinue -Verbose
+    New-Item /docker-transcodeautomation/data/logs -ItemType Directory -ErrorAction silentlycontinue -Verbose
+    New-Item /docker-transcodeautomation/data/Backups -ItemType Directory -ErrorAction silentlycontinue -Verbose
+
+    ##Import PSSqlite
+    Import-Module /root/.local/share/powershell/Modules/PSSQLite/1.1.0/PSSQLite.psm1 -ErrorAction Stop
+
+    ##Test for existence of media database
+    $datasource = ("/docker-transcodeautomation/data/MediaDB.SQLite")
+    $test3 = Test-Path $datasource
+    if ($test3 -eq $false) {
+        Write-Output "[+] Creating sqlite database"
+        . $PSScriptRoot/functions/Invoke-DBSetup.ps1
+        Invoke-DBSetup -DataSource "/docker-transcodeautomation/data/MediaDB.SQLite"
+    }
+
+    ##Update the database for missing tables added in new versions of docker-transcodeautomation
+    /docker-transcodeautomation/scripts/Update-Database.ps1
+
+    ##Create PS Drive for available free space checking
+    New-PSDrive -Name transcoding -Root /docker-transcodeautomation/transcoding -PSProvider FileSystem
+
+    ##Check for required variables
+    if ($null -eq $env:BACKUPPROCESSED -or $null -eq $env:BACKUPRETENTION -or $null -eq $env:MEDIAMOVIEFOLDERS -or $null -eq $env:MEDIASHOWFOLDERS) {
+        Write-Warning "[-] Required environment Variable not set. Review the README documentation for help. Processing will not continue."
+        while ($true) {
+            Start-Sleep -Seconds 2147483
+        }
+    }
+
+    ##Import transcode automation functions and continue with transcode automation
     ##Import Functions
     $FunctionPath = $PSScriptRoot + "\functions\"
     $scripts = (Get-ChildItem $FunctionPath).fullname
@@ -54,7 +55,7 @@ if ($host.version.major -eq '7') {
         . $script
     }
 
-    #set variables
+    ##set variables
     $backupfolder = "$PSScriptRoot/data/Backups"
     [string[]]$MEDIAmoviefolders = $env:MEDIAMOVIEFOLDERS -split ', '
     [string[]]$MEDIAshowfolders = $env:MEDIASHOWFOLDERS -split ', '
@@ -73,7 +74,7 @@ if ($host.version.major -eq '7') {
     }
 
     # Begin Automation
-    # If set update metadata of existing media only
+    ##If set update metadata of existing media only
     if ($env:UPDATEMETADATA -eq 'true') {
         /docker-transcodeautomation/scripts/Update-Metadata.ps1
         while ($true) {
@@ -81,16 +82,16 @@ if ($host.version.major -eq '7') {
         }
     }
 
-    # Else begin transcode processing
+    ##Else begin transcode processing
     else {
         while ($true) {
-            # Transcode Automation Execution
-            #begin processing
+            ###Transcode Automation Execution
+            ###begin processing
 
             if (Invoke-Timecompare -STARTTIMEUTC $env:STARTTIMEUTC -ENDTIMEUTC $env:ENDTIMEUTC) {
                 $dt = Get-Date
                 Write-Output "[+] Transcodeautomation while loop started at $dt"
-                # The Move-FiletoMediaFolder function is run here as a part of the fix for issue #29 Having it run here ensures even if there are no files to transcode, the failed file move is processed during the next window.
+                ###The Move-FiletoMediaFolder function is run here as a part of the fix for issue #29 Having it run here ensures even if there are no files to transcode, the failed file move is processed during the next window.
                 /docker-transcodeautomation/scripts/Update-Processed.ps1
                 Move-FileToMEDIAFolder -MEDIAshowfolders $MEDIAshowfolders -MEDIAmoviefolders $MEDIAmoviefolders -DataSource $datasource #Issue 29
                 Invoke-MediaManagement -hours $env:MINAGE -MEDIAshowfolders $MEDIAshowfolders -MEDIAmoviefolders $MEDIAmoviefolders -DataSource $datasource
