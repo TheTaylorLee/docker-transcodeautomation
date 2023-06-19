49c25cd70cfb04c93c53b35a2d1313e4bc98b39b
TheTaylorLee <53202926+TheTaylorLee@users.noreply.github.com>
2023-06-18 20:37:59 -0500

    Update Build Script

--- build.sh
+++ build.sh
@@ -1,19 +1,23 @@
-# CI/CD for maintainers only and for those trying to build their own image from the source
-## Build Dev Images
+# Build from source
 cd ~
 rm docker-transcodeautomation -d -r
 git clone https://github.com/thetaylorlee/docker-transcodeautomation
 cd docker-transcodeautomation
-version=v2.9.0
+version=$(cat version)
+
+## Build Dev
 #DOCKER_BUILDKIT=1 docker build -f Dockerfile.ubuntu22.04-lts -t ghcr.io/thetaylorlee/docker-transcodeautomation:ubuntu22.04-lts-develop .
 #DOCKER_BUILDKIT=1 docker build -f Dockerfile.alpine3.14-lts -t ghcr.io/thetaylorlee/docker-transcodeautomation:alpine3.14-lts-develop .
 
-### Build Prod
-#DOCKER_BUILDKIT=1 docker build -f Dockerfile.ubuntu22.04-lts -t ghcr.io/thetaylorlee/docker-transcodeautomation:ubuntu22.04-lts-$version .
-#DOCKER_BUILDKIT=1 docker build -f Dockerfile.alpine3.14-lts -t ghcr.io/thetaylorlee/docker-transcodeautomation:alpine3.14-lts-$version .
-#DOCKER_BUILDKIT=1 docker build -f Dockerfile.ubuntu22.04-lts -t ghcr.io/thetaylorlee/docker-transcodeautomation:ubuntu22.04-lts .
-#DOCKER_BUILDKIT=1 docker build -f Dockerfile.alpine3.14-lts -t ghcr.io/thetaylorlee/docker-transcodeautomation:alpine3.14-lts .
+## Build Prod
+### Ubuntu
+DOCKER_BUILDKIT=1 docker build -f Dockerfile.ubuntu22.04-lts -t ghcr.io/thetaylorlee/docker-transcodeautomation:ubuntu22.04-lts-$version .
+DOCKER_BUILDKIT=1 docker build -f Dockerfile.ubuntu22.04-lts -t ghcr.io/thetaylorlee/docker-transcodeautomation:ubuntu22.04-lts .
+
+### Alpine
+DOCKER_BUILDKIT=1 docker build -f Dockerfile.alpine3.14-lts -t ghcr.io/thetaylorlee/docker-transcodeautomation:alpine3.14-lts-$version .
+DOCKER_BUILDKIT=1 docker build -f Dockerfile.alpine3.14-lts -t ghcr.io/thetaylorlee/docker-transcodeautomation:alpine3.14-lts .
 DOCKER_BUILDKIT=1 docker build -f Dockerfile.alpine3.14-lts -t ghcr.io/thetaylorlee/docker-transcodeautomation:latest .
 
-### Pushing the images
+### Pushing the images for maintainers only. Currently managed by actions
 #docker push ghcr.io/thetaylorlee/docker-transcodeautomation -a
